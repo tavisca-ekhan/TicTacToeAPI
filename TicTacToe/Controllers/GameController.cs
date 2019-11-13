4a21282259b6a69ef71491125f5ba1335d3bb922
Khan <mdkhan@internal.tavisca.com>
2019-11-14 03:08:59 +0530

    Adding to Dev-branch

--- TicTacToe/Controllers/GameController.cs
+++ TicTacToe/Controllers/GameController.cs
@@ -4,29 +4,97 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TicTacToe.Models;
 
 namespace TicTacToe.Controllers
 {
     public class GameController : ApiController
     {
-        GameBoard gameBoard = new GameBoard();
+
+        static List<List<char>> board = new List<List<char>>()
+        {
+            new List<char>()
+            {
+                '-','-','-'
+            },
+            new List<char>()
+            {
+                '-','-','-'
+            },
+            new List<char>()
+            {
+                '-','-','-'
+            }
+        };
 
         // GET: api/Game
         public IEnumerable<IEnumerable<char>> Get()
         {
             // return new string[] { "value1", "value2" };
-            return gameBoard.CreateBoard();
+            return board;
+            
         }
 
         // GET: api/Game/5
         public string Get(int id)
         {
-            return "value";
+            return "value of id " + id;
         }
 
-        // POST: api/Game
-        public void Post([FromBody]string value)
+        // POST: api/Game/playerId
+        public string Post(int playerId, [FromBody]Vector vector)
         {
+            if (playerId == 1) return PlayGame('x', playerId, vector);
+            else if (playerId == 2) return PlayGame('o', playerId, vector);
+
+            if (IsDraw()) return "Game is Draw";
+
+            return "Next turn";
+        }
+
+        private bool IsDraw()
+        {
+            for (int row = 0; row < 3; row++)
+                for (int col = 0; col < 3; col++)
+                    if (board[row][col] == '-') return false;
+
+            return true;       
+        }
+
+        private string PlayGame(char ele, int playerId, Vector vector)
+        {
+            int row = vector.row;
+            int col = vector.col;
+
+            if (board[row][col] == '-')
+            {
+                board[row][col] = ele;
+                if (GameOver(ele))
+                    return "Player " + playerId + " wins";
+                return "Move Done \n Player " + (playerId == 1 ? playerId + 1 : playerId - 1) + " chance";
+            }
+            else
+            {
+                return "Invalid Move!!! Try Again";
+            }
+        }
+
+        private bool GameOver(char ch)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                if (board[0][row] == ch && board[1][row] == ch && board[2][row] == ch)
+                    return true;
+
+                if (board[row][0] == ch && board[row][1] == ch && board[row][2] == ch)
+                    return true;
+            }
+
+            if ((board[0][0] == ch && board[1][1] == ch && board[2][2] == ch) ||
+               (board[0][2] == ch && board[1][1] == ch && board[2][0] == ch))
+                return true;
+
+            return false;
         }
 
         // PUT: api/Game/5
@@ -37,7 +105,25 @@ namespace TicTacToe.Controllers
         // DELETE: api/Game
         public string Delete()
         {
-            gameBoard = new GameBoard().CreateBoard();
+            // Deletion done
+            board = new List<List<char>>()
+            {
+                new List<char>()
+                {
+                    '-','-','-'
+                },
+                new List<char>()
+                {
+                    '-','-','-'
+                },
+                new List<char>()
+                {
+                    '-','-','-'
+                }
+            };
+
+            return "The Board is Reset!";
         }
     }
+    
 }
